8846cd6dfd18fa0d4d0c6f795565ae2ebf565470
Alexy <alexy.ib@outlook.com>
2024-05-09 12:49:37 +0300

    Initial Commit

---
+++ Plugins/iOS/Kindly.xcframework/ios-arm64/Kindly.framework/Headers/Kindly.h
@@ -0,0 +1,18 @@
+//
+//  Kindly.h
+//  Kindly
+//
+//  Created by Alexy Ibrahim on 12/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kindly.
+FOUNDATION_EXPORT double KindlyVersionNumber;
+
+//! Project version string for Kindly.
+FOUNDATION_EXPORT const unsigned char KindlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kindly/PublicHeader.h>
+
+
